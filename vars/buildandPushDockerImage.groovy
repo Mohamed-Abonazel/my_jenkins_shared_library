0f8ec156b629d8335e33eacc11f5a449a2674b1b
Mohamed-Abonazel <mohamedabonazel4@gmail.com>
2025-01-07 00:18:02 +0200

    update buildandPushDockerimage

--- vars/buildandPushDockerImage.groovy
+++ vars/buildandPushDockerImage.groovy
@@ -1,13 +1,16 @@
 #!usr/bin/env groovy
 def call(String dockerHubCredentialsID, String imageName) {
-	// Log in to DockerHub 
-	withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-		sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-        }
-        
-        // Build and push Docker image
-        echo "Building and Pushing Docker image..."
-        sh "docker build -t ${imageName}:${BUILD_NUMBER} ."
-        sh "docker push ${imageName}:${BUILD_NUMBER}"	 
+    // Log in to DockerHub
+    withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh "echo ${PASSWORD} | docker login -u ${USERNAME} --password-stdin"
+    }
+    
+    // Ensure the project is built
+    sh './gradlew clean build'  // This will generate the JAR file in build/libs
+
+    // Build and push Docker image
+    echo "Building and Pushing Docker image..."
+    sh "docker build -t ${imageName}:${BUILD_NUMBER} ."
+    sh "docker push ${imageName}:${BUILD_NUMBER}"
 }
 
