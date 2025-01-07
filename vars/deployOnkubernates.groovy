e686d5f81cd807d595c9e807c157188c0707dfcd
Mohamed-Abonazel <mohamedabonazel4@gmail.com>
2025-01-07 20:13:40 +0200

    update deployOnkubernates

--- vars/deployOnkubernates.groovy
+++ vars/deployOnkubernates.groovy
@@ -1,18 +1,21 @@
 #!/usr/bin/env groovy
 
-def call(String k8sCredentialsID, String imageName) {
-
-    // Update deployment.yaml with new Docker Hub image
-    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
-
-    // Login to k8s Cluster via KubeConfig file
-    withCredentials([file(credentialsId: "${kubeconfigCredentialsID}", variable: 'KUBECONFIG_FILE')]) {
+def call(String kubeconfigCredentialsID, String kubernetesClusterURL, String imageName) {
+    sh """
+        sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml
+    """
+    
+    withCredentials([file(credentialsId: kubeconfigCredentialsID, variable: 'KUBECONFIG_FILE')]) {
         sh """
             export KUBECONFIG=${KUBECONFIG_FILE}
+            echo "Using Kubernetes Cluster at ${kubernetesClusterURL}"
+            kubectl cluster-info
             kubectl apply -f .
+            kubectl rollout status deployment/$(kubectl get deployment -o=jsonpath='{.items[0].metadata.name}')
         """
     }
+    
+    echo "Deployment to Kubernetes Cluster completed successfully."
 }
-
 
 
