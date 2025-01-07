5adf64fce2d523f330333b102eb23517e76d068f
Mohamed-Abonazel <mohamedabonazel4@gmail.com>
2025-01-07 18:23:20 +0200

    update deployOnkubernetes

--- vars/deployOnkubernates.groovy
+++ vars/deployOnkubernates.groovy
@@ -1,23 +1,18 @@
 #!/usr/bin/env groovy
 
-// kubeconfigCredentialsID refers to the Jenkins credential ID for the kubeconfig file.
-def call(String kubeconfigCredentialsID, String imageName) {
-    
-    // Update deployment.yaml with the new Docker image
+def call(String k8sCredentialsID, String imageName) {
+
+    // Update deployment.yaml with new Docker Hub image
     sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 
-    // Use Kubeconfig credentials to authenticate with Kubernetes
-    withCredentials([file(credentialsId: "${kubeconfigCredentialsID}", variable: 'KUBECONFIG')]) {
-        sh '''
-        export KUBECONFIG=${KUBECONFIG}
-        
-        # Check connection to the Kubernetes cluster
-        kubectl cluster-info
-
-        # Apply all YAML files in the current directory to the default namespace
-        kubectl apply -f .
-        '''
+    // Login to k8s Cluster via KubeConfig file
+    withCredentials([file(credentialsId: "${kubeconfigCredentialsID}", variable: 'KUBECONFIG_FILE')]) {
+        sh """
+            export KUBECONFIG=${KUBECONFIG_FILE}
+            kubectl apply -f .
+        """
     }
 }
+
 
 
