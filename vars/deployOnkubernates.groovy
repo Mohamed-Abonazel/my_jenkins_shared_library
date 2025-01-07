2ad9483b93e37240932671890a71b01efd1cd546
Mohamed-Abonazel <mohamedabonazel4@gmail.com>
2025-01-07 05:38:16 +0200

    update deployOnKubernates

--- vars/deployOnkubernates.groovy
+++ vars/deployOnkubernates.groovy
@@ -1,6 +1,26 @@
-#!usr/bin/env groovy
-def call(kubeconfigPath, deploymentYaml) {
-    echo "Deploying the application to Kubernetes"
-    sh "kubectl --kubeconfig=${kubeconfigPath} apply -f ${deploymentYaml}"
+#!/usr/bin/env groovy
+
+// kubeconfigCredentialsID refers to the Jenkins credential ID for the kubeconfig file.
+def call(String kubeconfigCredentialsID, String kubernetesClusterURL, String kubernetesNamespace, String imageName) {
+    
+    // Update deployment.yaml with the new Docker image
+    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
+
+    // Use Kubeconfig credentials to authenticate with Kubernetes
+    withCredentials([file(credentialsId: "${kubeconfigCredentialsID}", variable: 'KUBECONFIG')]) {
+        sh '''
+        export KUBECONFIG=${KUBECONFIG}
+        
+        # Check connection to the Kubernetes cluster
+        kubectl cluster-info
+
+        # Switch to the desired namespace
+        kubectl config set-context --current --namespace=${kubernetesNamespace}
+
+        # Apply all YAML files in the current directory
+        kubectl apply -f .
+        '''
+    }
 }
+
 
