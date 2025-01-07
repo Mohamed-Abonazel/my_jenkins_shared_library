293c58ce7bdfa595eb098340f011f906d03455a4
Mohamed-Abonazel <mohamedabonazel4@gmail.com>
2025-01-07 20:53:44 +0200

    update deployOnkubernates

--- vars/deployOnkubernates.groovy
+++ vars/deployOnkubernates.groovy
@@ -1,21 +1,23 @@
 #!/usr/bin/env groovy
 
 def call(String kubeconfigCredentialsID, String kubernetesClusterURL, String imageName) {
+    // Update the deployment.yaml image tag with the current build number
     sh """
         sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml
     """
     
+    // Use Kubernetes credentials to apply the deployment
     withCredentials([file(credentialsId: kubeconfigCredentialsID, variable: 'KUBECONFIG_FILE')]) {
         sh """
             export KUBECONFIG=${KUBECONFIG_FILE}
             echo "Using Kubernetes Cluster at ${kubernetesClusterURL}"
-            kubectl cluster-info
+            kubectl cluster-info --server=${kubernetesClusterURL}
             kubectl apply -f .
             kubectl rollout status deployment/$(kubectl get deployment -o=jsonpath='{.items[0].metadata.name}')
         """
     }
     
+    // Echo a success message to indicate deployment completion
     echo "Deployment to Kubernetes Cluster completed successfully."
 }
-
 
