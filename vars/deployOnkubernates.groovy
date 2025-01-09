749ffbe577eb6b1461cc489983e74396622c6f89
Mohamed-Abonazel <mohamedabonazel4@gmail.com>
2025-01-10 00:14:54 +0200

    update deployOnkubernetes

--- vars/deployOnkubernates.groovy
+++ vars/deployOnkubernates.groovy
@@ -2,7 +2,7 @@
 
 def call(String kubeconfigCredentialsID, String kubernetesClusterURL, String imageName, String caCertContent, String clientCertContent, String clientKeyContent) {
     // Define the path to deployment.yaml
-    def deploymentYamlPath = "Kubernetes/deployment.yaml"
+    def deploymentYamlPath = "CloudDevOpsProject-iVolve/Kubernetes/deployment.yaml"
     
     // Update the deployment.yaml image tag with the current build number
     sh """
