93b588e6f743cf9c119ac37d7cbe8a56cf110f87
Mohamed-Abonazel <mohamedabonazel4@gmail.com>
2025-01-10 03:13:30 +0200

    update deployonkubernates

--- vars/deployOnkubernates.groovy
+++ vars/deployOnkubernates.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
-def call(String kubeconfigCredentialsID, String kubernetesClusterURL, String imageName, String minikubeCACertID, String minikubeClientCertID , String minikubeClientKeyID) {
+def call(String kubeconfigCredentialsID, String kubernetesClusterURL, String imageName, String minikubeCACertID, String minikubeClientCertID, String minikubeClientKeyID) {
     // Define the path to deployment.yaml
     def deploymentYamlPath = "Kubernetes/deployment.yaml"
-    
+
     // Update the deployment.yaml image tag with the current build number
     sh """
         if [ -f ${deploymentYamlPath} ]; then
@@ -13,25 +13,23 @@ def call(String kubeconfigCredentialsID, String kubernetesClusterURL, String ima
             exit 1
         fi
     """
-    
+
     // Use Kubernetes credentials and certificates to apply the deployment
-    withCredentials([file(credentialsId: kubeconfigCredentialsID, variable: 'KUBECONFIG_FILE')]) 
+    withCredentials([file(credentialsId: kubeconfigCredentialsID, variable: 'KUBECONFIG_FILE')]) {
         // Export the KUBECONFIG and run cluster info
         sh """
             export KUBECONFIG=$KUBECONFIG_FILE
             kubectl cluster-info || { echo "Cluster info failed"; exit 1; }
         """
 
-         {
-            sh """
-                echo "Using Kubernetes Cluster at ${kubernetesClusterURL}"
-                kubectl cluster-info || { echo "Cluster info failed"; exit 1; }
-                kubectl apply -f ${deploymentYamlPath} || { echo "Kubectl apply failed"; exit 1; }
-                kubectl rollout status deployment/\$(kubectl get deployment -o=jsonpath='{.items[0].metadata.name}') || { echo "Rollout failed"; exit 1; }
-            """
-        }
-    
-    
+        // Apply the deployment using kubectl
+        sh """
+            echo "Using Kubernetes Cluster at ${kubernetesClusterURL}"
+            kubectl apply -f ${deploymentYamlPath} || { echo "Kubectl apply failed"; exit 1; }
+            kubectl rollout status deployment/\$(kubectl get deployment -o=jsonpath='{.items[0].metadata.name}') || { echo "Rollout failed"; exit 1; }
+        """
+    }
+
     // Echo a success message to indicate deployment completion
     echo "Deployment to Kubernetes Cluster completed successfully."
 }
